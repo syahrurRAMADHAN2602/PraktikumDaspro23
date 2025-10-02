94e5cac06450c1cf5f82a6ec6d65ce35a7ec98f6
SyahrurRamadhan <syahrurramadhan2609@gmail.com>
2025-10-02 12:44:15 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS23.java
+++ jobsheet5/ifCetakKRS23.java
@@ -16,6 +16,8 @@ public class ifCetakKRS23 {
         if (uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
 
     }
